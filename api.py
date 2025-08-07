253924700ef1cb7bebea751c39900f6988b42c09
camreyaro <camilareyyes@gmail.com>
2025-08-07 17:27:24 +0200

    extend api

--- api.py
+++ api.py
@@ -23,7 +23,7 @@ def login():
         'client_id': spotify_auth.client_id,
         'response_type': 'code',
         'redirect_uri': spotify_auth.redirect_uri,
-        'scope': 'user-library-read'
+        'scope': 'user-library-read user-top-read'
     }
     return RedirectResponse(f'https://accounts.spotify.com/authorize?{urlencode(params)}')
 
@@ -51,16 +51,78 @@ def callback(request: Request):
     return token_data
 
 # API
-@router.get('/')
-def hello_world():
-    return 'Hello world!'
 @router.get('/user_data')
 @spotify_auth.check_auth_token_timeout
 def user_data():
     r = httpx.get('https://api.spotify.com/v1/me',
-                   headers={'Authorization': f'Bearer {spotify_auth.access_token}'}).json()
+                   headers={'Authorization': f'Bearer {spotify_auth.auth_token}'})
 
+    r_data = r.json()
     return {
-        'name': r['display_name'],
-        'photo': r['images'][0]['url'],
+        'name': r_data['display_name'],
+        'photo': r_data['images'][0]['url'],
     }
+
+@router.get('/top_tracks')
+@spotify_auth.check_auth_token_timeout
+def top_tracks():
+    r = httpx.get('https://api.spotify.com/v1/me/top/tracks?limit=10',
+                   headers={'Authorization': f'Bearer {spotify_auth.auth_token}'})
+
+    r_data = r.json()
+    top_tracks = []
+
+    for item in r_data.get('items', []):
+        track_info = {
+            'name': item['name'],
+            'artist': ', '.join(artist['name'] for artist in item['artists']),
+            'album_image_url': item['album']['images'][0]['url'] if item['album']['images'] else None
+        }
+        top_tracks.append(track_info)
+
+    return top_tracks
+
+@router.get('/top_artists')
+@spotify_auth.check_auth_token_timeout
+def top_tracks():
+    r = httpx.get('https://api.spotify.com/v1/me/top/artists?limit=10',
+                   headers={'Authorization': f'Bearer {spotify_auth.auth_token}'})
+
+    r_data = r.json()
+    top_artists = []
+
+    for item in r_data.get('items', []):
+        artist_info = {
+            'name': item['name'],
+            'image_url': item['images'][0]['url'] if item['images'] else None
+        }
+        top_artists.append(artist_info)
+
+    return top_artists
+
+@router.get('/top_tracks_by_artist')
+@spotify_auth.check_auth_token_timeout
+def top_tracks_by_artist():
+    offset = 0
+    limit = 50
+    top_tracks_by_artist = {}
+
+    while offset <= 100:
+        r = httpx.get(f'https://api.spotify.com/v1/me/top/tracks?limit={limit}&offset={offset}',
+                       headers={'Authorization': f'Bearer {spotify_auth.auth_token}'})
+
+        r_data = r.json()
+
+        for item in r_data.get('items', []):
+            artists = [artist['name'] for artist in item['artists']]
+            for artist_name in artists:
+                if artist_name not in top_tracks_by_artist:
+                    top_tracks_by_artist[artist_name] = []
+
+                top_tracks_by_artist[artist_name].append({
+                    'name': item['name'],
+                    'album_image_url': item['album']['images'][0]['url'] if item['album']['images'] else None
+                })
+        offset += limit
+
+    return top_tracks_by_artist
