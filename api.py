5bce2aa499aba778f1b4125e99ecf00bdb1abaa2
camreyaro <camilareyyes@gmail.com>
2025-10-15 19:50:59 +0200

    allow multiple users

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from urllib.parse import urlencode
 import httpx
 from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
+from starlette.responses import JSONResponse
 
 from auth import SpotifyAuth
 
@@ -48,26 +49,46 @@ def callback(request: Request):
     spotify_auth.auth_token = token_data['access_token']
     spotify_auth.auth_refresh_token = token_data['refresh_token']
     spotify_auth.auth_token_timeout = token_data['expires_in']
-    return token_data
+
+    response = RedirectResponse("http://localhost:3000/stats")  # UI
+    response.set_cookie(
+        key="access_token",
+        value=token_data["access_token"],
+        httponly=True,
+        max_age=token_data["expires_in"],
+        secure=True,
+        samesite="lax"
+    )
+    return response
 
 # API
 @router.get('/user-data')
-@spotify_auth.check_auth_token_timeout
-def user_data():
+# @spotify_auth.check_auth_token_timeout
+def user_data(request: Request):
+    token = request.cookies.get('access_token')
+    if not token:
+        return JSONResponse({'error': 'No access token'}, status_code=401)
+
     r = httpx.get('https://api.spotify.com/v1/me',
-                   headers={'Authorization': f'Bearer {spotify_auth.auth_token}'})
+                   headers={'Authorization': f'Bearer {token}'})
 
     r_data = r.json()
+
     return {
-        'name': r_data['display_name'],
-        'photo': r_data['images'][0]['url'],
+        'name': r_data.get('display_name'),
+        'photo': r_data.get('images', [{}])[0].get("url")
     }
 
 @router.get('/top-tracks')
-@spotify_auth.check_auth_token_timeout
-def top_tracks():
-    r = httpx.get('https://api.spotify.com/v1/me/top/tracks?limit=10',
-                   headers={'Authorization': f'Bearer {spotify_auth.auth_token}'})
+# @spotify_auth.check_auth_token_timeout
+def top_tracks(request: Request):
+    token = request.cookies.get("access_token")
+    if not token:
+        return JSONResponse({"error": "No access token"}, status_code=401)
+
+    time_range = 'long_term'
+    r = httpx.get(f'https://api.spotify.com/v1/me/top/tracks?limit=10&time_range={time_range}',
+                   headers={'Authorization': f'Bearer {token}'})
 
     r_data = r.json()
     top_tracks = []
@@ -83,10 +104,15 @@ def top_tracks():
     return top_tracks
 
 @router.get('/top-artists')
-@spotify_auth.check_auth_token_timeout
-def top_tracks():
-    r = httpx.get('https://api.spotify.com/v1/me/top/artists?limit=10',
-                   headers={'Authorization': f'Bearer {spotify_auth.auth_token}'})
+# @spotify_auth.check_auth_token_timeout
+def top_artists(request: Request):
+    token = request.cookies.get("access_token")
+    if not token:
+        return JSONResponse({"error": "No access token"}, status_code=401)
+
+    time_range = 'long_term'
+    r = httpx.get(f'https://api.spotify.com/v1/me/top/artists?limit=10&time_range={time_range}',
+                   headers={'Authorization': f'Bearer {token}'})
 
     r_data = r.json()
     top_artists = []
@@ -101,19 +127,32 @@ def top_tracks():
     return top_artists
 
 @router.get('/top-tracks-by-artist')
-@spotify_auth.check_auth_token_timeout
-def top_tracks_by_artist():
+# @spotify_auth.check_auth_token_timeout
+def top_tracks_by_artist(request: Request):
+    token = request.cookies.get("access_token")
+    if not token:
+        return JSONResponse({"error": "No access token"}, status_code=401)
+
     offset = 0
     limit = 50
+    time_range = 'long_term'
     top_tracks_by_artist = {}
 
-    while offset <= 100:
-        r = httpx.get(f'https://api.spotify.com/v1/me/top/tracks?limit={limit}&offset={offset}',
-                       headers={'Authorization': f'Bearer {spotify_auth.auth_token}'})
+    seen_tracks = set()  # avoid repeated tracks
 
+    while offset <= 50:
+        r = httpx.get(
+            f'https://api.spotify.com/v1/me/top/tracks?limit={limit}&offset={offset}&time_range={time_range}',
+            headers={'Authorization': f'Bearer {token}'}
+        )
         r_data = r.json()
 
         for item in r_data.get('items', []):
+            track_id = item['id']
+            if track_id in seen_tracks:
+                continue
+            seen_tracks.add(track_id)
+
             artists = [artist['name'] for artist in item['artists']]
             for artist_name in artists:
                 if artist_name not in top_tracks_by_artist:
@@ -123,6 +162,7 @@ def top_tracks_by_artist():
                     'name': item['name'],
                     'album_image_url': item['album']['images'][0]['url'] if item['album']['images'] else None
                 })
+
         offset += limit
 
     return top_tracks_by_artist
